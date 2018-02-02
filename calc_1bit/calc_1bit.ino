650ae8a7da2ec1907eefa7907be1d41c9533e1f7
danielgalion <32830355+danielgalion@users.noreply.github.com>
2018-02-02 13:17:36 +0100

    Update calc_1bit.ino
    Code used to check if adder works. Results are displayed on diodes.

--- calc_1bit/calc_1bit.ino
+++ calc_1bit/calc_1bit.ino
@@ -1,56 +1,32 @@
 #define C_IN 2
-#define X 3
-#define Y 4
-#define C_OUT 13
-#define C_IN_BUTTON 10
-#define X_BUTTON 11
-#define Y_BUTTON 12
+#define X_0 3
+#define Y_0 4
+#define X_1 5
+#define Y_1 6
+#define X_2 7
+#define Y_2 8
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(C_IN, OUTPUT);
-  pinMode(X, OUTPUT);
-  pinMode(Y, OUTPUT);
-  pinMode(C_OUT, INPUT);
-  pinMode(C_IN_BUTTON, INPUT_PULLUP);
-  pinMode(X_BUTTON, INPUT_PULLUP);
-  pinMode(Y_BUTTON, INPUT_PULLUP);
-  digitalWrite(C_IN, LOW);
-  digitalWrite(X, LOW);
-  digitalWrite(Y, LOW);
-  Serial.begin(9600);
+  pinMode(X_0, OUTPUT);
+  pinMode(Y_0, OUTPUT);
+  pinMode(X_1, OUTPUT);
+  pinMode(Y_1, OUTPUT);
+  pinMode(X_2, OUTPUT);
+  pinMode(Y_2, OUTPUT);
+
+  digitalWrite(C_IN, HIGH);
+  digitalWrite(X_0, HIGH);
+  digitalWrite(Y_0, HIGH);
+  digitalWrite(X_1, HIGH);
+  digitalWrite(Y_1, HIGH);
+  digitalWrite(X_2, HIGH);
+  digitalWrite(Y_2, HIGH);
 }
 
-int number = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(digitalRead(C_IN_BUTTON) == LOW) {
-    number = 0;
-    delay(20);
-  } else if (digitalRead(X_BUTTON) == LOW) {
-    number = 1;
-    delay(20);
-  } else if (digitalRead(Y_BUTTON) == LOW) {
-    number = 2;
-    delay(20);
-  } else {
-    number = 3;
-    delay(20);
-  }
 
-  switch (number) {
-    case 0:
-      Serial.println("C_IN");
-      break;
-    case 1:
-      Serial.println("X");
-      break;
-    case 2:
-      Serial.println("Y");
-      break;
-    case 3:
-      Serial.println("0");
-      break;
-    }
 }
